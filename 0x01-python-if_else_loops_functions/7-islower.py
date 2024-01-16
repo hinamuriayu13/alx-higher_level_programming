297c3e1b36c3d67b2d7a96f1ad7679be87df21fd
hinamuriayu13 <harukanyan1@gmail.com>
2024-01-16 02:24:43 -0800

    mssg

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -9,3 +9,9 @@ print("Q is {}".format("lower" if islower("Q") else "upper"))
 print("Z is {}".format("lower" if islower("Z") else "upper"))
 print("7 is {}".format("lower" if islower("7") else "upper"))
 print("f is {}".format("lower" if islower("f") else "upper"))
+print("'4' is {}".format("lower" if islower("4") else "upper"))
+print("'!' is {}".format("lower" if islower("!") else "upper"))
+print("'a' is {}".format("lower" if islower("a") else "upper")) 
+print("'H' is {}".format("lower" if islower("H") else "upper"))
+
+
