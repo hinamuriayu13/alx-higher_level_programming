d6e13fd22dc535d0f39fa9519f53b35c67e8472b
hinamuriayu13 <harukanyan1@gmail.com>
2024-01-16 02:56:51 -0800

    mssg

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,3 @@
 def print_last_digit(number):
     last_digit = abs(number) % 10
     print(last_digit, end='')
-
-# Test cases
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
