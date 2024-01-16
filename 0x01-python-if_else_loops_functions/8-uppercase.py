44a260afa6b7378d5ab41b17d6d74cb1e6c2b49d
hinamuriayu13 <harukanyan1@gmail.com>
2024-01-16 02:42:54 -0800

    mssg

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    for char in str:
+        ascii_value = ord(char)
+        if 97 <= ascii_value <= 122:
+            uppercase_char = chr(ascii_value - 32)
+        else:
+            uppercase_char = char
+        print(uppercase_char, end="")
+    print()
