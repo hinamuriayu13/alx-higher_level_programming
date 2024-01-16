6197e1357e66b852dc2a99fcf7ce461b8e3ce93e
hinamuriayu13 <harukanyan1@gmail.com>
2024-01-16 01:54:02 -0800

    msg

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,30 +8,14 @@ last_digit = abs(number) % 10
 
 # Check if the number is positive, negative, or zero
 if last_digit > 5:
-    print(
-        "Last digit of", number,
-        "is", last_digit, "and is greater than 5"
-    )
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
     if number < 0:
         print(number, "is negative")
-    elif number > 0:
-        print(number, "is positive")
     else:
-        print(number, "is zero")
+        print(number, "is positive")
 elif last_digit < 6 and last_digit != 0:
-    print(
-        "Last digit of", number,
-        "is", last_digit, "and is less than 6 and not 0"
-    )
-    if number < 0:
-        print(number, "is negative")
-    elif number > 0:
-        print(number, "is positive")
-    else:
-        print(number, "is zero")
-else:
-    print(
-        "Last digit of", number,
-        "is", last_digit, "and is 0"
-    )
+    print("Last digit of", number, "is", -last_digit, "and is less than 6 and not 0")
+    print(number, "is negative")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0")
     print(number, "is zero")
