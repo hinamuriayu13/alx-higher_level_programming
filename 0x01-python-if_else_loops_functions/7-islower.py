5c0508cddef7567f7ca4dcc48aae7923fe51654a
hinamuriayu13 <harukanyan1@gmail.com>
2024-01-16 02:27:19 -0800

    mssg

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+# Filename: 7-islower.py
+
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
 
-# Test cases with different characters
+
+# Test cases
 print("x is {}".format("lower" if islower("x") else "upper"))
 print("Q is {}".format("lower" if islower("Q") else "upper"))
 print("Z is {}".format("lower" if islower("Z") else "upper"))
@@ -11,7 +14,5 @@ print("7 is {}".format("lower" if islower("7") else "upper"))
 print("f is {}".format("lower" if islower("f") else "upper"))
 print("'4' is {}".format("lower" if islower("4") else "upper"))
 print("'!' is {}".format("lower" if islower("!") else "upper"))
-print("'a' is {}".format("lower" if islower("a") else "upper")) 
+print("'a' is {}".format("lower" if islower("a") else "upper"))
 print("'H' is {}".format("lower" if islower("H") else "upper"))
-
-
